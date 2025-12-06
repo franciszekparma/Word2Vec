d844d733138a16caacdbd925b2d6bdd55a7ff72e
franciszekparma <Franciszek.parma@gmail.com>
2025-12-06 21:22:23 +0100

    Added .gitignore  and data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -1,6 +1,22 @@
-from datasets import load_dataset, load_from_disk
+from datasets import Dataset, load_dataset
+import pyarrow as pa
 
 
 dataset = load_dataset("embedding-data/flickr30k_captions_quintets")
-
 dataset.save_to_disk("dataset/flickr30k_captions")
+dataset = Dataset.from_file("dataset/flickr30k_captions/train/data-00000-of-00001.arrow")
+
+all_sentences = []
+for example in dataset:
+  all_sentences.extend(example['set'])
+  
+all_words = []
+for sentence in all_sentences:
+  words_in_sen = sentence.split()
+  all_words.extend(words_in_sen)
+  
+vocab = list(set(all_words)) #getting only the unique words (no duplicates)
+
+print(f"Total sentences: {len(all_sentences)}")
+print(f"Total words (with duplicates): {len(all_words)}")
+print(f"Vocab size: {len(vocab)}")
